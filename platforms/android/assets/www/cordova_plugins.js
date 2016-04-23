e1699da0e64b22f1dd1caab68456ce74eb483fcc
Noa Avigad <noa3179@gmail.com>
2016-04-23 12:11:38 -0700

    sms-plugin

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -1,9 +1,18 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-sms/www/SMS.js",
+        "id": "cordova-plugin-sms.SMS",
+        "clobbers": [
+            "window.SMS"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.2.2"
+    "cordova-plugin-whitelist": "1.2.2",
+    "cordova-plugin-sms": "1.0.5"
 };
 // BOTTOM OF METADATA
 });
