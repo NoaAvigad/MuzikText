aaf6616d9db5d0a175e3a09d56ace81c5137ecb3
Larissa Feng <hello@larissafeng.me>
2016-04-23 15:50:01 -0700

    Added event listener for text input

--- www/js/index.js
+++ www/js/index.js
@@ -26,26 +26,13 @@ var app = {
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
+        document.getElementById("sendTextButton").addEventListener('click', this.sendText, false);
     },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+    sendText: function() {
+        var textToSend = document.getElementById('textToSend').value;
+        document.getElementById('textSentDiv').innerHTML = "Text to send to Project Oxford is: " + textToSend;
+        document.getElementById('textSentDiv').style.visibility = 'visible';
     },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
 };
 
 app.initialize();
